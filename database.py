218752cb8eda2777be7d9a4e0a00b2d34f6d4197
yashin7code <yashin.code@gmail.com>
2026-07-27 08:12:33 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres:admin123@localhost/yashindb"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
